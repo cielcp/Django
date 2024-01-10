a1e09eb2c5ee593080e075206dc4518cc7ce0d9b
cielcp <cielcpark@gmail.com>
2024-01-10 03:55:31 -0500

    big push

---
+++ polls/views.py
@@ -0,0 +1,101 @@
+from django.http import HttpResponseRedirect
+from django.shortcuts import get_object_or_404, render
+from django.urls import reverse
+from django.views import generic
+from django.utils import timezone
+from django.forms import ModelForm, inlineformset_factory
+from django.forms.widgets import HiddenInput
+
+from .models import Choice, Question
+
+# VIEWS
+class IndexView(generic.ListView):
+    template_name = "polls/index.html"
+    context_object_name = "latest_question_list"
+
+    def get_queryset(self):
+        """
+        Return the last five published questions (not including those set to be
+        published in the future).
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now()).order_by("-pub_date")[ :5]
+
+class DetailView(generic.DetailView):
+    model = Question
+    template_name = "polls/detail.html"
+
+    def get_queryset(self):
+        """
+        Excludes any questions that aren't published yet.
+        """
+        return Question.objects.filter(pub_date__lte=timezone.now())
+
+class ResultsView(generic.DetailView):
+    model = Question
+    template_name = "polls/results.html"
+
+def vote(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    try:
+        selected_choice = question.choice_set.get(pk=request.POST["choice"])
+    except (KeyError, Choice.DoesNotExist):
+        # Redisplay the question voting form.
+        return render(
+            request,
+            "polls/detail.html",
+            {
+                "question": question,
+                "error_message": "You didn't select a choice.",
+            },
+        )
+    else:
+        selected_choice.votes += 1
+        selected_choice.save()
+        # Always return an HttpResponseRedirect after successfully dealing
+        # with POST data. This prevents data from being posted twice if a
+        # user hits the Back button.
+        return HttpResponseRedirect(reverse("polls:results", args=(question.id,)))
+    
+# PART 4
+
+class ChoiceForm(ModelForm):
+    class Meta:
+        model = Choice
+        #fields = ["question", "choice_text", "votes"]
+        exclude = ["votes"]
+
+class QuestionForm(ModelForm):
+    class Meta:
+        model = Question
+        # fields = ["question_text", "pub_date"]
+        exclude = ["pub_date"]
+
+class AddView(generic.CreateView):
+    model = Question
+    template_name = "polls/add.html"
+    # fields = ["question_text", "pub_date"]
+    form_class = QuestionForm
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        #ChoiceFormSet = inlineformset_factory(Question, Choice, fields=["choice_text"])
+        ChoiceFormSet = inlineformset_factory(Question, Choice, form=ChoiceForm)
+        context['formset'] = ChoiceFormSet(self.request.POST if self.request.method == 'POST' else None)
+        return context
+
+    def form_valid(self, form):
+        context = self.get_context_data()
+        formset = context['formset']
+
+        if form.is_valid() and formset.is_valid():
+            question = form.save(commit=False)
+            question.pub_date = timezone.now()
+            question.save()
+
+            # Associate choices with the question and save them
+            formset.instance = question
+            # formset.votes = 0
+            formset.save()
+            # If successful, redirect to updated list of questions.
+            return HttpResponseRedirect(reverse("polls:index"))
+        return self.form_invalid(form)
